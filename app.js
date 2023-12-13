6116e1af18d15dfb6c1ba62ff40550366db9b7fa
AsimMertTopal <mertopal@icloud.com>
2023-12-13 16:15:26 +0300

    Yapılan güncelleme

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-import  express  from "express";
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const app =express()
-const port=3000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.static('public'));
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/",(req,res) => {
-    res.render("index")
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Port Çalışıyor: ${port}`);
-  });
+});
