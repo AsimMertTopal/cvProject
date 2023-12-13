a448dff6431f105cab0f62d6af4e61e00b2404ce
AsimMertTopal <mertopal@icloud.com>
2023-12-13 19:01:34 +0300

    fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
